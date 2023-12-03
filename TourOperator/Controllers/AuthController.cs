328ce7b39f0094c18eb758192055214dfa34967e
Rhys Adams <rhys@therhys.co.uk>
2023-12-03 15:32:44 +0000

    Initial commit.

---
+++ TourOperator/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using TourOperator.Models;
+
+namespace TourOperator.Controllers;
+
+[ApiController]
+[Route("/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly ILogger<HomeController> _logger;
+    IConfiguration? Configuration { get; }
+
+    public AuthController(IConfiguration configuration, ILogger<HomeController> logger)
+    {
+        _logger = logger;
+        Configuration = configuration;
+    }
+
+    [HttpGet("/login")]
+    public IActionResult Login()
+    {
+        string connectionString = Configuration?["ConnectionStrings:DefaultConnection"] ?? "";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        return Ok("Success");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return Problem("Error");
+    }
+}
---
+++ TourOperator/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using TourOperator.Models;
+
+namespace TourOperator.Controllers;
+
+[ApiController]
+[Route("/")]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    IConfiguration? Configuration { get; }
+
+    public HomeController(IConfiguration configuration, ILogger<HomeController> logger)
+    {
+        _logger = logger;
+        Configuration = configuration;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        string connectionString = Configuration?["ConnectionStrings:DefaultConnection"] ?? "";
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        return View();
+    }
+
+    [HttpGet("/privacy")]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
