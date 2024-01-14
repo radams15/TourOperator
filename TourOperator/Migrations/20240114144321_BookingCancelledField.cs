1ad33d575a13372c7292218070f67b3d66dc73fa
Rhys Adams <rhys@therhys.co.uk>
2024-01-14 14:54:27 +0000

    Add migrations.

---
+++ TourOperator/Migrations/20240114144321_BookingCancelledField.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TourOperator.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookingCancelledField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Customers",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Cancelled",
+                table: "Bookings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Username",
+                table: "Customers",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_Username",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Cancelled",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(32)",
+                oldMaxLength: 32);
+        }
+    }
+}
