42cf197a99172b6f61a66c74e3d063afe06828bf
Rhys Adams <rhys@therhys.co.uk>
2024-01-16 10:43:22 +0000

    Comment extensions.

--- TourOperator/Extensions/StringExtensions.cs
+++ TourOperator/Extensions/StringExtensions.cs
@@ -5,6 +5,11 @@ using System.Text;
 
 public static class StringExtensions
 {
+    /// <summary>
+    /// Sha256 method to hash a string.
+    /// </summary>
+    /// <param name="rawData">String to hash</param>
+    /// <returns>Sha256 hash of the string</returns>
     public static string Sha256(this string rawData)
     {
         using (SHA256 sha256Hash = SHA256.Create())
